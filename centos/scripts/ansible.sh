726c3ab92bd264f5d952c75cffd02f07ea4f678e
Kevin Hannan <kphannan@gmail.com>
2018-07-14 07:00:50 -0400

    Added ansible to the base image

--- centos/scripts/ansible.sh
+++ centos/scripts/ansible.sh
@@ -2,5 +2,5 @@
 
 ## Install ansible to allow for ansible_local
 if [[ $INSTALL_ANSIBLE  =~ true || $INSTALL_ANSIBLE =~ 1 || $INSTALL_ANSIBLE =~ yes ]]; then
-    yum -y install ansible
+    yum -y install ansible libselinux-python
 fi
